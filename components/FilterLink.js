da96b1f5bc697bcfbd3e0625999ddac90bcfa00d
Sky Chen <skychen@yahoo-inc.com>
2016-07-25 22:54:15 -0700

    remove unused code

--- components/FilterLink.js
+++ components/FilterLink.js
@@ -27,41 +27,5 @@ const FilterLink = connect(
   mapStateToProps,
   mapDispatchToProps
 )(Link);
-/*
-class FilterLink extends Component {
-  componentDidMount() {
-    this.unsubscribe = this.context.store.subscribe(() =>
-      this.forceUpdate()
-    );
-  }
-  componentWillUnmount() {
-    this.unsubscribe();
-  }
-  render() {
-    const props = this.props;
-    const { store } = this.context;
-    const state = store.getState();
 
-    return (
-      <Link
-        active={
-          props.filter === state.visibilityFilter
-        }
-        onClick={() =>
-          store.dispatch({
-            type: 'SET_VISIBILITY_FILTER',
-            filter: props.filter
-          })
-        }
-      >
-        {props.children}
-      </Link>
-    );
-  }
-}
-
-FilterLink.contextTypes = {
-  store: PropTypes.object
-};
-*/
 export default FilterLink;
